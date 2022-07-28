efcd567cf847f66e3f8ae3f222a8b2f5e83f8ac5
Heloisepetit <110096644+Heloisepetit@users.noreply.github.com>
2022-07-28 12:37:43 +0200

    Update des.py

--- modules/des.py
+++ modules/des.py
@@ -2,7 +2,7 @@ import random
 
 class tirage_des:
   def tirage(self):
-    le_tirage = random.randint(2,2)
+    le_tirage = random.randint(0,6)
     return le_tirage
   def verification(self,choix_joueur,tirage):
     if choix_joueur == str(tirage):
