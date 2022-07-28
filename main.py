8833597f6c49112f8db9bf74683aa6d6c3ec0b3f
HeloisePETIT <110096644+Heloisepetit@users.noreply.github.com>
2022-07-28 09:40:37 +0000

    bouh

--- main.py
+++ main.py
@@ -14,5 +14,5 @@ def jeux():
 	resultat = des.tirage_des().verification(choix_joueur, mon_tirage)
 	print(choix_joueur)
 	return render_template('page_jeu.html', resu=resultat, tira=mon_tirage)
-
+#commentaire
 #app.run(host='0.0.0.0', port=81)
